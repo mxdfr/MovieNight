3e02d59d24bc1c3444d3d3020b7d75bfd8cfd493
Lucas de Geus <lucasdegeus1999@gmail.com>
2018-10-27 15:45:15 +0200

    comments

--- main.js
+++ main.js
@@ -2,20 +2,7 @@ angular.module('KRRclass', [ 'chart.js']).controller('MainCtrl', ['$scope','$htt
 
 
 function mainCtrl($scope, $http, ChartJsProvider){
-  	
-	 
-	
-	// ###################### Question 4 
-	// Associate your sparql endpoint to a scope variable. Add the "/query?query=" parameter after the variable
-	// Ex. $scope.myVariable = "myendpoint"
 	$scope.mysparqlendpoint = "http://localhost:5820/FinalProjectKD/query?query=";
-	
-
-
-	// ###################### Question 6 	
-	// Create an interaction with the triplestore by filling the following method 
-	// The function needs to include : some arguments sent by the html + an http call to the sparql endpoint + a variable storing the results to be visualised    
-	// use the native function encodeURI(mySparqlQuery) to encode your query as a URL
 	$scope.fireInteraction = function(){
 		$http( {
  			method: "GET",
@@ -24,12 +11,8 @@ function mainCtrl($scope, $http, ChartJsProvider){
 			} )
 			.success(function(data, status ) {
           $scope.result = data;
-          // // Use the console to see how your response looks like
-// console.log(data);
 $scope.myDynamicLabels = [];
 $scope.myDynamicData = [];
-				  
- // now iterate on the results
 angular.forEach(data.results.bindings, function(val) {
 	$scope.myDynamicLabels.push(val.movie.value);
 	$scope.myDynamicData.push(val.rating.value);
